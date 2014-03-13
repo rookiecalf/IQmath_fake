de19b157c113cf364469a120c15a72cae81a2c08
rookiecalf <liuchangjiang.liu@gmail.com>
2014-03-13 12:43:00 +0800

    添加IQmath的除法IQdiv测试
    直接返回期望值的方式通过测试

---
+++ mocks/IQmath_Arithmetic/IQmath_Division.c
@@ -0,0 +1,24 @@
+/**********************************************************************
+//-    Copyright (c) Changjiang. Liu -- All Rights Reserved         
+//-    Licensed under the Apache License, Version 2.0 (the "License").   
+//-                                                                  
+//-    Available at https://github.com/rookiecalf/IQmath_fake             
+//-                
+//-                                                                   
+//-    Authorized users may use this source code in your own          
+//-    projects. We make no guarantees that this source code is       
+//-    fit for any purpose.                                           
+//-                                                                   
+//-    softci.com    rookiecalf@gmail.com      
+**********************************************************************/
+#include "math.h"
+
+long _IQ1div(long A, long B, int num)
+{
+	A = A ;
+	B= B ;
+	num = num ;
+
+	return 3 ;
+}
+
