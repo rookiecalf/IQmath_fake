1d01d2fb85823de9c1d980a129f82550f215f224
rookiecalf <liuchangjiang.liu@gmail.com>
2014-03-13 16:02:41 +0800

    完成IQsin 3-29的测试替身，并通过测试用例

--- mocks/IQmath_Trigonometric/IQmath_Sine.c
+++ mocks/IQmath_Trigonometric/IQmath_Sine.c
@@ -32,3 +32,137 @@ long _IQ2sin(long A)
 	return __IQsin(A, 2) ;
 }
 
+long _IQ3sin(long A)
+{
+	return __IQsin(A, 3) ;
+}
+
+long _IQ4sin(long A)
+{
+	return __IQsin(A, 4) ;
+}
+
+long _IQ5sin(long A)
+{
+	return __IQsin(A, 5) ;
+}
+
+long _IQ6sin(long A)
+{
+	return __IQsin(A, 6) ;
+}
+
+long _IQ7sin(long A)
+{
+	return __IQsin(A, 7) ;
+}
+
+long _IQ8sin(long A)
+{
+	return __IQsin(A, 8) ;
+}
+
+long _IQ9sin(long A)
+{
+	return __IQsin(A, 9) ;
+}
+
+long _IQ10sin(long A)
+{
+	return __IQsin(A, 10) ;
+}
+
+long _IQ11sin(long A)
+{
+	return __IQsin(A, 11) ;
+}
+
+long _IQ12sin(long A)
+{
+	return __IQsin(A, 12) ;
+}
+
+long _IQ13sin(long A)
+{
+	return __IQsin(A, 13) ;
+}
+
+long _IQ14sin(long A)
+{
+	return __IQsin(A, 14) ;
+}
+
+long _IQ15sin(long A)
+{
+	return __IQsin(A, 15) ;
+}
+
+long _IQ16sin(long A)
+{
+	return __IQsin(A, 16) ;
+}
+
+long _IQ17sin(long A)
+{
+	return __IQsin(A, 17) ;
+}
+
+long _IQ18sin(long A)
+{
+	return __IQsin(A, 18) ;
+}
+
+long _IQ19sin(long A)
+{
+	return __IQsin(A, 19) ;
+}
+
+long _IQ20sin(long A)
+{
+	return __IQsin(A, 20) ;
+}
+
+long _IQ21sin(long A)
+{
+	return __IQsin(A, 21) ;
+}
+
+long _IQ22sin(long A)
+{
+	return __IQsin(A, 22) ;
+}
+
+long _IQ23sin(long A)
+{
+	return __IQsin(A, 23) ;
+}
+
+long _IQ24sin(long A)
+{
+	return __IQsin(A, 24) ;
+}
+
+long _IQ25sin(long A)
+{
+	return __IQsin(A, 25) ;
+}
+
+long _IQ26sin(long A)
+{
+	return __IQsin(A, 26) ;
+}
+
+long _IQ27sin(long A)
+{
+	return __IQsin(A, 27) ;
+}
+
+long _IQ28sin(long A)
+{
+	return __IQsin(A, 28) ;
+}
+
+long _IQ29sin(long A)
+{
+	return __IQsin(A, 29) ;
+}
