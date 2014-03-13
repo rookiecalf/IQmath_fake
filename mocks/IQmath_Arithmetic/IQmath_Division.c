3e8c3ae48a811228e25d1ad44e871a50fc6e6b02
rookiecalf <liuchangjiang.liu@gmail.com>
2014-03-13 13:48:54 +0800

    添加IQ3-30测试替身，并进行测试
    Q3-29使用简单的测试用例，Q30进行了边界测试

--- mocks/IQmath_Arithmetic/IQmath_Division.c
+++ mocks/IQmath_Arithmetic/IQmath_Division.c
@@ -28,3 +28,143 @@ long _IQ2div(long A, long B)
 {
 	return __IQdiv(A, B, 2) ;
 }
+
+long _IQ3div(long A, long B)
+{
+	return __IQdiv(A, B, 3) ;
+}
+
+long _IQ4div(long A, long B)
+{
+	return __IQdiv(A, B, 4) ;
+}
+
+long _IQ5div(long A, long B)
+{
+	return __IQdiv(A, B, 5) ;
+}
+
+long _IQ6div(long A, long B)
+{
+	return __IQdiv(A, B, 6) ;
+}
+
+long _IQ7div(long A, long B)
+{
+	return __IQdiv(A, B, 7) ;
+}
+
+long _IQ8div(long A, long B)
+{
+	return __IQdiv(A, B, 8) ;
+}
+
+long _IQ9div(long A, long B)
+{
+	return __IQdiv(A, B, 9) ;
+}
+
+long _IQ10div(long A, long B)
+{
+	return __IQdiv(A, B, 10) ;
+}
+
+long _IQ11div(long A, long B)
+{
+	return __IQdiv(A, B, 11) ;
+}
+
+long _IQ12div(long A, long B)
+{
+	return __IQdiv(A, B, 12) ;
+}
+
+long _IQ13div(long A, long B)
+{
+	return __IQdiv(A, B, 13) ;
+}
+
+long _IQ14div(long A, long B)
+{
+	return __IQdiv(A, B, 14) ;
+}
+
+long _IQ15div(long A, long B)
+{
+	return __IQdiv(A, B, 15) ;
+}
+
+long _IQ16div(long A, long B)
+{
+	return __IQdiv(A, B, 16) ;
+}
+
+long _IQ17div(long A, long B)
+{
+	return __IQdiv(A, B, 17) ;
+}
+
+long _IQ18div(long A, long B)
+{
+	return __IQdiv(A, B, 18) ;
+}
+
+long _IQ19div(long A, long B)
+{
+	return __IQdiv(A, B, 19) ;
+}
+
+long _IQ20div(long A, long B)
+{
+	return __IQdiv(A, B, 20) ;
+}
+
+long _IQ21div(long A, long B)
+{
+	return __IQdiv(A, B, 21) ;
+}
+
+long _IQ22div(long A, long B)
+{
+	return __IQdiv(A, B, 22) ;
+}
+
+long _IQ23div(long A, long B)
+{
+	return __IQdiv(A, B, 23) ;
+}
+
+long _IQ24div(long A, long B)
+{
+	return __IQdiv(A, B, 24) ;
+}
+
+long _IQ25div(long A, long B)
+{
+	return __IQdiv(A, B, 25) ;
+}
+
+long _IQ26div(long A, long B)
+{
+	return __IQdiv(A, B, 26) ;
+}
+
+long _IQ27div(long A, long B)
+{
+	return __IQdiv(A, B, 27) ;
+}
+
+long _IQ28div(long A, long B)
+{
+	return __IQdiv(A, B, 28) ;
+}
+
+long _IQ29div(long A, long B)
+{
+	return __IQdiv(A, B, 29) ;
+}
+
+long _IQ30div(long A, long B)
+{
+	return __IQdiv(A, B, 30) ;
+}
--- tests/IQmath_Arithmetic/IQmath_Division.cpp
+++ tests/IQmath_Arithmetic/IQmath_Division.cpp
@@ -59,3 +59,149 @@ TEST(IQmath_Division, IQ2divNegative)
     LONGS_EQUAL(_IQ2(-16268815.48), _IQ2div(_IQ2(-536870911), _IQ2(33))) ;
 }
 
+TEST(IQmath_Division, IQ3div)
+{
+    LONGS_EQUAL(_IQ3(1.5), _IQ3div(_IQ3(-3.0), _IQ3(-2.0))) ;
+}
+
+TEST(IQmath_Division, IQ4div)
+{
+    LONGS_EQUAL(_IQ4(1.5), _IQ4div(_IQ4(-3.0), _IQ4(-2.0))) ;
+}
+
+TEST(IQmath_Division, IQ5div)
+{
+    LONGS_EQUAL(_IQ5(1.5), _IQ5div(_IQ5(-3.0), _IQ5(-2.0))) ;
+}
+
+TEST(IQmath_Division, IQ6div)
+{
+    LONGS_EQUAL(_IQ6(1.5), _IQ6div(_IQ6(-3.0), _IQ6(-2.0))) ;
+}
+
+TEST(IQmath_Division, IQ7div)
+{
+    LONGS_EQUAL(_IQ7(1.5), _IQ7div(_IQ7(-3.0), _IQ7(-2.0))) ;
+}
+
+TEST(IQmath_Division, IQ8div)
+{
+    LONGS_EQUAL(_IQ8(1.5), _IQ8div(_IQ8(-3.0), _IQ8(-2.0))) ;
+}
+
+TEST(IQmath_Division, IQ9div)
+{
+    LONGS_EQUAL(_IQ9(1.5), _IQ9div(_IQ9(-3.0), _IQ9(-2.0))) ;
+}
+
+TEST(IQmath_Division, IQ10div)
+{
+    LONGS_EQUAL(_IQ10(1.5), _IQ10div(_IQ10(-3.0), _IQ10(-2.0))) ;
+}
+
+TEST(IQmath_Division, IQ11div)
+{
+    LONGS_EQUAL(_IQ11(1.5), _IQ11div(_IQ11(-3.0), _IQ11(-2.0))) ;
+}
+
+TEST(IQmath_Division, IQ12div)
+{
+    LONGS_EQUAL(_IQ12(1.5), _IQ12div(_IQ12(-3.0), _IQ12(-2.0))) ;
+}
+
+TEST(IQmath_Division, IQ13div)
+{
+    LONGS_EQUAL(_IQ13(1.5), _IQ13div(_IQ13(-3.0), _IQ13(-2.0))) ;
+}
+
+TEST(IQmath_Division, IQ14div)
+{
+    LONGS_EQUAL(_IQ14(1.5), _IQ14div(_IQ14(-3.0), _IQ14(-2.0))) ;
+}
+
+TEST(IQmath_Division, IQ15div)
+{
+    LONGS_EQUAL(_IQ15(1.5), _IQ15div(_IQ15(-3.0), _IQ15(-2.0))) ;
+}
+
+TEST(IQmath_Division, IQ16div)
+{
+    LONGS_EQUAL(_IQ16(1.5), _IQ16div(_IQ16(-3.0), _IQ16(-2.0))) ;
+}
+
+TEST(IQmath_Division, IQ17div)
+{
+    LONGS_EQUAL(_IQ17(1.5), _IQ17div(_IQ17(-3.0), _IQ17(-2.0))) ;
+}
+
+TEST(IQmath_Division, IQ18div)
+{
+    LONGS_EQUAL(_IQ18(1.5), _IQ18div(_IQ18(-3.0), _IQ18(-2.0))) ;
+}
+
+TEST(IQmath_Division, IQ19div)
+{
+    LONGS_EQUAL(_IQ19(1.5), _IQ19div(_IQ19(-3.0), _IQ19(-2.0))) ;
+}
+
+TEST(IQmath_Division, IQ20div)
+{
+    LONGS_EQUAL(_IQ20(1.5), _IQ20div(_IQ20(-3.0), _IQ20(-2.0))) ;
+}
+
+TEST(IQmath_Division, IQ21div)
+{
+    LONGS_EQUAL(_IQ21(1.5), _IQ21div(_IQ21(-3.0), _IQ21(-2.0))) ;
+}
+
+TEST(IQmath_Division, IQ22div)
+{
+    LONGS_EQUAL(_IQ22(1.5), _IQ22div(_IQ22(-3.0), _IQ22(-2.0))) ;
+}
+
+TEST(IQmath_Division, IQ23div)
+{
+    LONGS_EQUAL(_IQ23(1.5), _IQ23div(_IQ23(-3.0), _IQ23(-2.0))) ;
+}
+
+TEST(IQmath_Division, IQ24div)
+{
+    LONGS_EQUAL(_IQ24(1.5), _IQ24div(_IQ24(-3.0), _IQ24(-2.0))) ;
+}
+
+TEST(IQmath_Division, IQ25div)
+{
+    LONGS_EQUAL(_IQ25(1.5), _IQ25div(_IQ25(-3.0), _IQ25(-2.0))) ;
+}
+
+TEST(IQmath_Division, IQ26div)
+{
+    LONGS_EQUAL(_IQ26(1.5), _IQ26div(_IQ26(-3.0), _IQ26(-2.0))) ;
+}
+
+TEST(IQmath_Division, IQ27div)
+{
+    LONGS_EQUAL(_IQ27(1.5), _IQ27div(_IQ27(-3.0), _IQ27(-2.0))) ;
+}
+
+TEST(IQmath_Division, IQ28div)
+{
+    LONGS_EQUAL(_IQ28(1.5), _IQ28div(_IQ28(-3.0), _IQ28(-2.0))) ;
+}
+
+TEST(IQmath_Division, IQ29div)
+{
+    LONGS_EQUAL(_IQ29(1.5), _IQ29div(_IQ29(-3.0), _IQ29(-2.0))) ;
+}
+
+TEST(IQmath_Division, IQ30div)
+{
+    LONGS_EQUAL(_IQ30(1.5), _IQ30div(_IQ30(1.5), _IQ30(1))) ;
+    LONGS_EQUAL(_IQ30(1.42857142785), _IQ30div(_IQ30(1.999999999), _IQ30(1.4))) ;
+}
+
+TEST(IQmath_Division, IQ30divNegative)
+{
+    LONGS_EQUAL(_IQ30(1.5), _IQ30div(_IQ30(-1.5), _IQ30(-1))) ;
+    LONGS_EQUAL(_IQ30(-1.42857142785), _IQ30div(_IQ30(-1.999999999), _IQ30(1.4))) ;
+}
