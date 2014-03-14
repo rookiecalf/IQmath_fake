1718ee843319c5d0b37d81b6f50781100383b1d1
rookiecalf <liuchangjiang.liu@gmail.com>
2014-03-14 13:09:46 +0800

    完成IQ3-30frac测试替身开发

--- mocks/IQmath_FormatConversion/IQmath_ExtractFraction.c
+++ mocks/IQmath_FormatConversion/IQmath_ExtractFraction.c
@@ -36,3 +36,143 @@ long _IQ2frac(long A)
 {
 	return __IQfrac(A, 2) ;
 }
+
+long _IQ3frac(long A)
+{
+	return __IQfrac(A, 3) ;
+}
+
+long _IQ4frac(long A)
+{
+	return __IQfrac(A, 4) ;
+}
+
+long _IQ5frac(long A)
+{
+	return __IQfrac(A, 5) ;
+}
+
+long _IQ6frac(long A)
+{
+	return __IQfrac(A, 6) ;
+}
+
+long _IQ7frac(long A)
+{
+	return __IQfrac(A, 7) ;
+}
+
+long _IQ8frac(long A)
+{
+	return __IQfrac(A, 8) ;
+}
+
+long _IQ9frac(long A)
+{
+	return __IQfrac(A, 9) ;
+}
+
+long _IQ10frac(long A)
+{
+	return __IQfrac(A, 10) ;
+}
+
+long _IQ11frac(long A)
+{
+	return __IQfrac(A, 11) ;
+}
+
+long _IQ12frac(long A)
+{
+	return __IQfrac(A, 12) ;
+}
+
+long _IQ13frac(long A)
+{
+	return __IQfrac(A, 13) ;
+}
+
+long _IQ14frac(long A)
+{
+	return __IQfrac(A, 14) ;
+}
+
+long _IQ15frac(long A)
+{
+	return __IQfrac(A, 15) ;
+}
+
+long _IQ16frac(long A)
+{
+	return __IQfrac(A, 16) ;
+}
+
+long _IQ17frac(long A)
+{
+	return __IQfrac(A, 17) ;
+}
+
+long _IQ18frac(long A)
+{
+	return __IQfrac(A, 18) ;
+}
+
+long _IQ19frac(long A)
+{
+	return __IQfrac(A, 19) ;
+}
+
+long _IQ20frac(long A)
+{
+	return __IQfrac(A, 20) ;
+}
+
+long _IQ21frac(long A)
+{
+	return __IQfrac(A, 21) ;
+}
+
+long _IQ22frac(long A)
+{
+	return __IQfrac(A, 22) ;
+}
+
+long _IQ23frac(long A)
+{
+	return __IQfrac(A, 23) ;
+}
+
+long _IQ24frac(long A)
+{
+	return __IQfrac(A, 24) ;
+}
+
+long _IQ25frac(long A)
+{
+	return __IQfrac(A, 25) ;
+}
+
+long _IQ26frac(long A)
+{
+	return __IQfrac(A, 26) ;
+}
+
+long _IQ27frac(long A)
+{
+	return __IQfrac(A, 27) ;
+}
+
+long _IQ28frac(long A)
+{
+	return __IQfrac(A, 28) ;
+}
+
+long _IQ29frac(long A)
+{
+	return __IQfrac(A, 29) ;
+}
+
+long _IQ30frac(long A)
+{
+	return __IQfrac(A, 30) ;
+}
